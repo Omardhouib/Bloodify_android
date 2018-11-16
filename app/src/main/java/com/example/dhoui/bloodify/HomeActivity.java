482835e72b82f2aea496ecc56f3d6b00c9cb1cf1
OmarDhouib <omar.dhouib@esprit.tn>
2018-11-17 00:14:40 +0100

    first

---
+++ app/src/main/java/com/example/dhoui/bloodify/HomeActivity.java
@@ -0,0 +1,31 @@
+package com.example.dhoui.bloodify;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("nom");
+        String username = intent.getStringExtra("Email");
+        int age = intent.getIntExtra("age", -1);
+
+        TextView tvWelcomeMsg = (TextView) findViewById(R.id.tvWelcomeMsg);
+        EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        EditText etAge = (EditText) findViewById(R.id.etAge);
+
+        // Display user details
+        String message = name + " welcome to your user area";
+        tvWelcomeMsg.setText(message);
+        etUsername.setText(username);
+        etAge.setText(age + "");
+    }
+}
---
+++ app/src/main/java/com/example/dhoui/bloodify/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.dhoui.bloodify;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bar);
+
+
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFragement()).commit();
+        }
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragement();
+                            break;
+                        case R.id.nav_message:
+                            selectedFragment = new MessageFragement();
+                            break;
+                        case R.id.nav_tip:
+                            selectedFragment = new TipFragement();
+                            break;
+                        case R.id.nav_hospital:
+                            selectedFragment = new HospitalFragement();
+                            break;
+                        case R.id.nav_profile:
+                            selectedFragment = new ProfileFragement();
+
+
+
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+
+}
